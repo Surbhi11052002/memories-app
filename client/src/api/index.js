21d2d785bcdd4cd43361a9a47b37b018d352eff1
Surbhi <surbhiprasad31@gmail.com>
2022-11-03 13:24:30 +0530

    added first post

---
+++ client/src/api/index.js
@@ -0,0 +1,6 @@
+import axios from "axios"; //we will use it to make api calls
+
+const url = "http://localhost:5000/posts";
+
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
